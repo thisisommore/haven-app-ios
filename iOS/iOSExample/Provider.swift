b0d473053f0122bd2d47d1a375863d8ab50a84f6
thisisommore <ommore501@gmail.com>
2025-12-16 19:58:06 +0530

    extract Provider and Root views into separate files

---
+++ iOS/iOSExample/Provider.swift
@@ -0,0 +1,59 @@
+//
+//  Provider.swift
+//  iOSExample
+//
+//  Created by Om More on 16/12/25.
+//
+
+import SwiftUI
+import SwiftData
+struct Provider<Content: View>: View {
+    @StateObject private var logOutput = LogViewer()
+    @StateObject private var xxdk = XXDK()
+    @StateObject private var secretManager = SecretManager()
+    @StateObject private var navigation = AppNavigationPath()
+    @StateObject private var selectedChat = SelectedChat()
+
+    @ViewBuilder let content: Content
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    private var modelContainer: ModelContainer = {
+        let schema = Schema([
+            ChatModel.self,
+            ChatMessageModel.self,
+            MessageReactionModel.self,
+            MessageSenderModel.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema)
+
+        do {
+            let modelContainer = try ModelContainer(
+                for: schema,
+                configurations: [modelConfiguration]
+            )
+            return modelContainer
+        } catch {
+            fatalError(
+                "Could not create ModelContainer: \(error.localizedDescription)"
+            )
+        }
+    }()
+
+    private var modelDataActor: SwiftDataActor {
+        SwiftDataActor(modelContainer: modelContainer)
+    }
+
+    var body: some View {
+        content
+            .modelContainer(modelContainer)
+            .environmentObject(logOutput)
+            .environmentObject(secretManager)
+            .environmentObject(xxdk)
+            .environmentObject(selectedChat)
+            .environmentObject(navigation)
+            .environmentObject(modelDataActor)
+    }
+}
