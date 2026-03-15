de52b8b115307f5f93eb348a4cd897becc81107a
thisisommore <ommore501@gmail.com>
2026-03-15 15:19:09 +0530

    MessageSenderModel: default id to new UUID at property declaration

--- iOS/iOSExample/Data/Model/MessageSenderModel.swift
+++ iOS/iOSExample/Data/Model/MessageSenderModel.swift
@@ -3,7 +3,7 @@ import SQLiteData
 
 @Table("messageSenders")
 struct MessageSenderModel {
-  var id: UUID
+  var id: UUID = .init()
   var pubkey: Data
   /// codename
   var codename: String
@@ -17,7 +17,6 @@ struct MessageSenderModel {
     pubkey: Data, codename: String, nickname: String? = nil, dmToken: Int32 = 0,
     color: Int
   ) {
-    self.id = UUID()
     self.pubkey = pubkey
     self.codename = codename
     self.nickname = nickname
