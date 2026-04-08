db493195b7c3d19996d1265487314e43a17fce8f
thisisommore <ommore501@gmail.com>
2026-04-08 09:10:12 +0530

    fix prod build

--- iOS/iOSExample/UI/PreviewUtils/PreviewUtils.swift
+++ iOS/iOSExample/UI/PreviewUtils/PreviewUtils.swift
@@ -4,90 +4,87 @@
 //
 //  Created by Om More on 22/12/25.
 //
-#if DEBUG
 
-  import Dependencies
-  import Foundation
-  import SQLiteData
-  import SwiftUI
+import Dependencies
+import Foundation
+import SQLiteData
+import SwiftUI
 
-  let previewChatId = UUID()
-  let previewChannelId = "previewChatId"
-  let greenColorInt = 0x0B421F
+let previewChatId = UUID()
+let previewChannelId = "previewChatId"
+let greenColorInt = 0x0B421F
 
-  /// Preview wrapper: seeds DB and dependencies before `content` is built (DEBUG only).
-  struct Mock<Content: View>: View {
-    @ViewBuilder let content: () -> Content
-    private let database: any DatabaseWriter
+/// Preview wrapper: seeds DB and dependencies before `content` is built (DEBUG only).
+struct Mock<Content: View>: View {
+  @ViewBuilder let content: () -> Content
+  private let database: any DatabaseWriter
 
-    init(@ViewBuilder content: @escaping () -> Content) {
-      self.content = content
-      let db = try! appDatabase()
-      self.database = db
-      try! Self.seedPreviewDatabase(db)
-    }
+  init(@ViewBuilder content: @escaping () -> Content) {
+    self.content = content
+    let db = try! appDatabase()
+    self.database = db
+    try! Self.seedPreviewDatabase(db)
+  }
 
-    var body: some View {
-      withDependencies {
-        $0.defaultDatabase = self.database
-      } operation: {
-        NavigationStack {
-          self.content()
-        }
-        .environmentObject(XXDKMock())
-        .environmentObject(SelectedChat())
-        .navigationBarBackButtonHidden()
+  var body: some View {
+    withDependencies {
+      $0.defaultDatabase = self.database
+    } operation: {
+      NavigationStack {
+        self.content()
       }
-    }
-
-    private static func seedPreviewDatabase(_ database: any DatabaseWriter) throws {
-      var chat = ChatModel(channelId: previewChannelId, name: "Mayur")
-      chat.id = previewChatId
-      let mockSender = MessageSenderModel(
-        pubkey: Data(), codename: "Mayur", nickname: nil, dmToken: nil,
-        color: greenColorInt
-      )
-
-      try database.write { db in
-        try ChatModel.insert { chat }.execute(db)
-        try MessageSenderModel.insert { mockSender }.execute(db)
-
-        let mockMsgs = previewMockMessages(chatId: chat.id, senderId: mockSender.id)
-
-        for msg in mockMsgs {
-          try ChatMessageModel.insert { msg }.execute(db)
-        }
-
-        try MessageReactionModel.insert {
-          MessageReactionModel(
-            id: InternalIdGenerator.shared.next(),
-            externalId: "wow",
-            targetMessageId: "4TDppExKKwB/pAvRNkCn9pGDi8IGPIGhJSKdouDoCqE=",
-            emoji: "💚",
-            senderId: mockSender.id
-          )
-        }.execute(db)
-
-        for name in ["ChatModel.selfChatInternalName", "Tom", "Shashank"] {
-          let dmChat = ChatModel(
-            pubKey: name.data, name: name, dmToken: 0, color: greenColorInt
-          )
-          try ChatModel.insert { dmChat }.execute(db)
-          try ChatMessageModel.insert {
-            ChatMessageModel(
-              message: "<p>Hello world</p>",
-              isIncoming: true,
-              chatId: dmChat.id,
-              id: InternalIdGenerator.shared.next(),
-              externalId: name,
-              replyTo: nil,
-              timestamp: Date(),
-              status: .delivered
-            )
-          }.execute(db)
-        }
-      }
+      .environmentObject(XXDKMock())
+      .environmentObject(SelectedChat())
+      .navigationBarBackButtonHidden()
     }
   }
 
-#endif
+  private static func seedPreviewDatabase(_ database: any DatabaseWriter) throws {
+    var chat = ChatModel(channelId: previewChannelId, name: "Mayur")
+    chat.id = previewChatId
+    let mockSender = MessageSenderModel(
+      pubkey: Data(), codename: "Mayur", nickname: nil, dmToken: nil,
+      color: greenColorInt
+    )
+
+    try database.write { db in
+      try ChatModel.insert { chat }.execute(db)
+      try MessageSenderModel.insert { mockSender }.execute(db)
+
+      let mockMsgs = previewMockMessages(chatId: chat.id, senderId: mockSender.id)
+
+      for msg in mockMsgs {
+        try ChatMessageModel.insert { msg }.execute(db)
+      }
+
+      try MessageReactionModel.insert {
+        MessageReactionModel(
+          id: InternalIdGenerator.shared.next(),
+          externalId: "wow",
+          targetMessageId: "4TDppExKKwB/pAvRNkCn9pGDi8IGPIGhJSKdouDoCqE=",
+          emoji: "💚",
+          senderId: mockSender.id
+        )
+      }.execute(db)
+
+      for name in ["ChatModel.selfChatInternalName", "Tom", "Shashank"] {
+        let dmChat = ChatModel(
+          pubKey: name.data, name: name, dmToken: 0, color: greenColorInt
+        )
+        try ChatModel.insert { dmChat }.execute(db)
+        try ChatMessageModel.insert {
+          ChatMessageModel(
+            message: "<p>Hello world</p>",
+            isIncoming: true,
+            chatId: dmChat.id,
+            id: InternalIdGenerator.shared.next(),
+            externalId: name,
+            replyTo: nil,
+            timestamp: Date(),
+            status: .delivered
+          )
+        }.execute(db)
+      }
+    }
+  }
+}
